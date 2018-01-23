aa3eb7eeca50afde7b238ba7eef2ca7ecafd8f3b
christelva <cvanaanholt@hotmail.com>
2018-01-23 16:47:40 +0100

    add seed data reducer

---
+++ react-game-starter-master/src/reducers/students.js
@@ -0,0 +1,602 @@
+[
+  {
+    "firstName": "Tamra",
+    "lastName": "Wickenden",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Archibald",
+    "lastName": "Steer",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Field",
+    "lastName": "Colquyte",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Lovell",
+    "lastName": "Le Frank",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Tim",
+    "lastName": "Dewerson",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Wilmer",
+    "lastName": "Moncrefe",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Cissy",
+    "lastName": "Zini",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Dory",
+    "lastName": "Treamayne",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Madelyn",
+    "lastName": "Pither",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Leigh",
+    "lastName": "Smartman",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Torrence",
+    "lastName": "Warner",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Gherardo",
+    "lastName": "Milillo",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Ken",
+    "lastName": "Davidow",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Cherey",
+    "lastName": "Raggatt",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Nolana",
+    "lastName": "Chipman",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Wain",
+    "lastName": "Winterton",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Missie",
+    "lastName": "Moreinis",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Brennan",
+    "lastName": "Swannick",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Ray",
+    "lastName": "Gorger",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Rhodia",
+    "lastName": "Gehringer",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Branden",
+    "lastName": "Garces",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Tanhya",
+    "lastName": "Duckworth",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Damara",
+    "lastName": "Woodwind",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Storm",
+    "lastName": "Allon",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Mendy",
+    "lastName": "Towll",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Marna",
+    "lastName": "Crabtree",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Prue",
+    "lastName": "Pizzie",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Haley",
+    "lastName": "Flinders",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Stefa",
+    "lastName": "Jorge",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Alyse",
+    "lastName": "Ogilby",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Lacy",
+    "lastName": "Gurys",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Loella",
+    "lastName": "Castlake",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Moise",
+    "lastName": "McQuillan",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Bren",
+    "lastName": "Sweetlove",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Eileen",
+    "lastName": "Flamank",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Auguste",
+    "lastName": "Driutti",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Jean",
+    "lastName": "Meiklam",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Shawnee",
+    "lastName": "Webburn",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Hurleigh",
+    "lastName": "Plett",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Rowena",
+    "lastName": "Jameson",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Aldon",
+    "lastName": "Sutterfield",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Barnie",
+    "lastName": "O'Scandall",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Barri",
+    "lastName": "Bolle",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Allie",
+    "lastName": "Corro",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Jacquette",
+    "lastName": "Wigginton",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Griswold",
+    "lastName": "Wragge",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Oneida",
+    "lastName": "Elbourne",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Cecilio",
+    "lastName": "Warriner",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Kahlil",
+    "lastName": "Slograve",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Reinaldo",
+    "lastName": "Lashmore",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Antoinette",
+    "lastName": "Devaney",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Bibbie",
+    "lastName": "Thunderchief",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Frederick",
+    "lastName": "Bauduccio",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Elle",
+    "lastName": "Mallam",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Evaleen",
+    "lastName": "Legendre",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Madelene",
+    "lastName": "Crone",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Virgie",
+    "lastName": "Critchell",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Dwain",
+    "lastName": "Ungaretti",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Gustavo",
+    "lastName": "Manoch",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Gunter",
+    "lastName": "M'cowis",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Allianora",
+    "lastName": "Rickardes",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Tildie",
+    "lastName": "Farny",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Melitta",
+    "lastName": "Forgan",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Imojean",
+    "lastName": "Dockwra",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Patrice",
+    "lastName": "Taffley",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Shirl",
+    "lastName": "Hutfield",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Dugald",
+    "lastName": "Cosely",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Grange",
+    "lastName": "Dudlestone",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Peggy",
+    "lastName": "Steere",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Rosalia",
+    "lastName": "Chipchase",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Rick",
+    "lastName": "Westhead",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Hall",
+    "lastName": "Margrem",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Rita",
+    "lastName": "Hamnet",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Aileen",
+    "lastName": "Daughton",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Denny",
+    "lastName": "Ion",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Rose",
+    "lastName": "Handslip",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Georgy",
+    "lastName": "Wimsett",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Romy",
+    "lastName": "Kydd",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Fairlie",
+    "lastName": "Bleackly",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Eleonora",
+    "lastName": "Flanner",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Justine",
+    "lastName": "Blackly",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Val",
+    "lastName": "Garken",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Gabi",
+    "lastName": "Roadnight",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Pris",
+    "lastName": "Cream",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Dugald",
+    "lastName": "Downing",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Dosi",
+    "lastName": "Naisby",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Putnam",
+    "lastName": "Dumingos",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Almeda",
+    "lastName": "McGuinley",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/dddddd/000000"
+  },
+  {
+    "firstName": "Chrissie",
+    "lastName": "Dorkins",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Corbet",
+    "lastName": "Deares",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Katherine",
+    "lastName": "Cocksedge",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Bjorn",
+    "lastName": "Gigg",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Wynny",
+    "lastName": "Illston",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Jillian",
+    "lastName": "Gland",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Bennie",
+    "lastName": "McKennan",
+    "batch": 1,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Catlin",
+    "lastName": "Percifer",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/5fa2dd/ffffff"
+  },
+  {
+    "firstName": "Emmerich",
+    "lastName": "Lening",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  },
+  {
+    "firstName": "Clifford",
+    "lastName": "Mendenhall",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Osgood",
+    "lastName": "Nuttey",
+    "batch": 3,
+    "picture": "http://dummyimage.com/100x100.jpg/cc0000/ffffff"
+  },
+  {
+    "firstName": "Leslie",
+    "lastName": "Powney",
+    "batch": 2,
+    "picture": "http://dummyimage.com/100x100.jpg/ff4444/ffffff"
+  }
+]
