b85cd3f606364335ba8cdc1adeb65a0c2222b941
christelva <cvanaanholt@hotmail.com>
2018-01-25 11:16:59 +0100

    import batchescontainer

--- evaluation-tool-app/src/App.js
+++ evaluation-tool-app/src/App.js
@@ -5,6 +5,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import muiTheme from './styles/theme'
 import Navigation from './components/UI/Navigation'
 import Routes from './routes'
+import BatchesContainer from './components/batches/BatchesContainer'
 
 const batches = [
   { "name": "Batch #1",
